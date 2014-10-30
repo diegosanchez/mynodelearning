4aa5d1beac1655a8ba4b04104fb4801a170cc87d
Diego Sanchez <diego.sanches@gmail.com>
2014-10-30 20:12:08 -0300

    Record requests

--- chapter_20/hello_world_app.js
+++ chapter_20/hello_world_app.js
@@ -2,6 +2,7 @@ var connect = require('connect');
 var midwear01 = require('./hello_world');
 var midwear02 = require('./welcome');
 var midwear03 = require('./http_header');
+var midwear04 = require('./write_request');
 
 var helloWorldMiddleweare01 = require('./hello_world');
 var port = Number(process.argv[2] || 8080);
@@ -9,6 +10,7 @@ var port = Number(process.argv[2] || 8080);
 var app = connect();
 
 app.use( midwear03('X-Powered-By', 'Node') );
+app.use( midwear04() );
 
 // Flush
 app.use( function( req, res, next) {
