643a87d971fbae4e373b3098a4873db469a598e8
diegosanchez <diego.sanches@gmail.com>
2014-10-31 16:54:05 -0300

    PROBLEM

--- chapter_19/server.js
+++ chapter_19/server.js
@@ -8,15 +8,6 @@ app.listen( port );
 console.log("Server listening on: ", port );
 
 function handler(req, res) {
-  var body = "";
-
-  req.on( 'data', function(data) {
-    body += data.toString();
-  });
-
-  req.on( 'end', function() {
-    var number = parseInt(body.trim() );
-    var result = JSON.stringify( Math.pow(number, 2) ) ;
-    res.end( result );
-  });
+  console.log( "Incomming: " , new Date() );
+  req.pipe(res);
 }
