617941a983a6b1d5ae6425513c93e30cde84d06d
diegosanchez <diego.sanches@gmail.com>
2014-11-05 11:51:59 -0300

    Adding comments for posts

--- flapperNews/app.js
+++ flapperNews/app.js
@@ -1,4 +1,23 @@
-app = angular.module( 'flapperNews', [] );
+app = angular.module( 'flapperNews', ['ui.router'] );
+
+app.config( ['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'home.html',
+      controller: 'MainCtrl'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: 'posts.html',
+      controller: 'PostCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
+
+app.controller( 'PostCtrl', ['$scope', 'posts', function ( $scope, posts ) {
+}]);
 
 app.controller( 'MainCtrl', ['$scope', 'posts', function ( $scope, posts ) {
   $scope.clearPostForm = function () {
