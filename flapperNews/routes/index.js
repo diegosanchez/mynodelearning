93016caaf712ff51b2de465565efaaf6e006bd85
diegosanchez <diego.sanches@gmail.com>
2014-11-06 16:55:49 -0300

    Posts & Comments routes added

--- flapperNews/routes/index.js
+++ flapperNews/routes/index.js
@@ -1,9 +1,30 @@
+var mongoose = require('mongoose');
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+router.get( '/posts', function(req, res, next) {
+	Post.find( function(err, posts ) {
+		if (err) { return next(err); };
+
+		res.json(posts);
+	})
 });
+
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
+});
+
+router.get( '/', function(req, res, next) {
+	res.render( 'index' );
+})
 
 module.exports = router;
